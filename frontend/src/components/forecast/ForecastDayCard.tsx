0bea7f3732a1300d42e8c5c1b04d03b9464e5725
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-03-15 22:07:12 +0200

    styles for active ForecastDayCard

--- frontend/src/components/forecast/ForecastDayCard.tsx
+++ frontend/src/components/forecast/ForecastDayCard.tsx
@@ -1,7 +1,16 @@
 import Card from "react-bootstrap/Card";
 
-const ForecastDayCard: React.FC = () => {
+type ForecastDayCardType = {
+  isActive: boolean;
+  toggleActive: () => void;
+};
+
+const ForecastDayCard: React.FC<ForecastDayCardType> = ({
+  isActive,
+  toggleActive,
+}) => {
   const forecast = {
+    city: "London",
     date_str: "Today", // 15th, 16th...
     icon_url: `https://openweathermap.org/img/wn/10d@2x.png`,
     temp_day_min: "9°",
@@ -13,7 +22,12 @@ const ForecastDayCard: React.FC = () => {
 
   return (
     <>
-      <Card className="forecast-card">
+      <Card
+        className={
+          isActive ? "forecast-card forecast-card--active" : "forecast-card"
+        }
+        onClick={e => toggleActive()}
+      >
         <Card.Body>
           <p>{forecast.date_str}</p>
           <div className="forecast-card__content">
@@ -24,6 +38,11 @@ const ForecastDayCard: React.FC = () => {
               <div>{forecast.temp_day_min}</div>
               <div>{forecast.temp_day_max}</div>
             </div>
+            {isActive && (
+              <div className="forecast-card__description">
+                {forecast.weather_description}
+              </div>
+            )}
           </div>
         </Card.Body>
         <Card.Footer>
