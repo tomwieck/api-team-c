7166db721f308acb6551e1c8e9f5a0e9bb13011a
OlgaPrikhodko <helga.prikhodko@gmail.com>
2023-03-15 19:08:28 +0200

    Revert "Frontend forecast components"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,41 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Button } from "react-bootstrap";
-import NavBar from "./components/NavBar";
-import ForecastCity from "./components/forecast/ForecastCity";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">API Team C</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
       <div>Animation for loading...</div>
       <div>
         <input type="text"></input>
         <Button>Find weather</Button>
       </div>
-      <ForecastCity />
+      <div>Weather for today, for tomorrow, day after tomorrow ...</div>
     </div>
   );
 }
