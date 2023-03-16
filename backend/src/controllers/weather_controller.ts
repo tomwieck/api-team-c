57043d8904de132466e64745a811372b63b1a09d
Julian <mrjrhirst@gmail.com>
2023-03-16 19:15:23 +0000

    Refactored to use .map and added 5 days summery stats: perceptprec, total_rain and total_snow

--- backend/src/controllers/weather_controller.ts
+++ backend/src/controllers/weather_controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { start } from "repl";
 import { cities } from '../database/database';
 
 export const getCities = async (req: Request, res: Response) => {
@@ -23,13 +24,35 @@ export const getCityFiveDayForecast = async (req: Request, res: Response) => {
       if (!city) {
         res.status(404).json("Unknown city.");
       } else {
+
+        interface ApiDaily {
+          weather: {
+            icon: string;
+            main: string;
+            description: string;
+          }[];
+          rain?: number;
+          snow?: number;
+          uvi: number;
+          wind_deg: number;
+          wind_speed: number;
+          temp: {
+            day: number;
+            min: number;
+            max: number;
+          };
+          pop: number;
+          clouds: number;
+          sunrise: number;
+          sunset: number;
+          humidity: number;
+        }
+
         const response = await fetch(
           `https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&exclude=hourly&appid=${process.env.WEATHER_API_KEY}&units=metric`
         );
         const apiData = await response.json();
-        const apiDaily = apiData.daily;
-
-        console.log(apiDaily, apiDaily.weather);
+        const apiDaily: ApiDaily[] = apiData.daily;
 
         interface OneDaysData {
           weather_title: String;
@@ -40,7 +63,7 @@ export const getCityFiveDayForecast = async (req: Request, res: Response) => {
           sunset_time: number;
           hours_daylight: number;
           uv_index: number;
-          wind_direction: String;
+          wind_direction: number;
           wind_speed: number;
           temp_day_noon: number;
           temp_day_min: number;
@@ -55,42 +78,45 @@ export const getCityFiveDayForecast = async (req: Request, res: Response) => {
         interface FiveDaysData {
           cityName: String;
           date: Number;
-          // percentprec: number;
-          // total_rain: number;
-          // total_snow: number;
-          // uv_index: Number;
+          percentprec: number;
+          total_rain: number;
+          total_snow: number;
           daily: OneDaysData[];
         }
 
-        let days: OneDaysData[] = [];
+        const days: OneDaysData[] = apiDaily.map((day) => {
 
-        for (let i = 0; i < 5; i++) {
-          days.push( {
-            icon_id: apiDaily[i].weather[0].icon as String,
-            icon_url: `http://openweather/icons/${apiDaily[i].weather[0].icon}.jpg`,
-            weather_title: apiDaily[i].weather[0].main,
-            weather_description: apiDaily[i].weather[0].description,
-            expected_rain: apiDaily[i].rain,
-            expected_snow: apiDaily[i].snow,
-            uv_index: apiDaily[i].uvi,
-            wind_direction: apiDaily[i].wind_deg,
-            wind_speed: apiDaily[i].wind_speed,
-            temp_day_noon: apiDaily[i].temp.day,
-            temp_day_min: apiDaily[i].temp.min,
-            temp_day_max: apiDaily[i].temp.max,
-            percentprec: apiDaily[i].pop,
-            cloud_cover: apiDaily[i].clouds,
-            sunrise_time: apiDaily[i].sunrise,
-            sunset_time: apiDaily[i].sunset,
-            hours_daylight: apiDaily[i].sunset - apiDaily.sunrise,
-            humidity: apiDaily[i].humidity
-          });
-        }
+          const weather = day.weather[0];
 
-        const data:FiveDaysData = {
+          return {
+            icon_id: weather.icon as string,
+            icon_url: `http://openweather/icons/${weather.icon}.jpg`,
+            weather_title: weather.main,
+            weather_description: weather.description,
+            expected_rain: day.rain ?? -1,
+            expected_snow: day.snow ?? -1,
+            uv_index: day.uvi,
+            wind_direction: day.wind_deg,
+            wind_speed: day.wind_speed,
+            temp_day_noon: day.temp.day,
+            temp_day_min: day.temp.min,
+            temp_day_max: day.temp.max,
+            percentprec: day.pop*100,
+            cloud_cover: day.clouds*100,
+            sunrise_time: day.sunrise,
+            sunset_time: day.sunset,
+            hours_daylight: day.sunset - day.sunrise,
+            humidity: day.humidity,
+          }
+        });
+
+        const data: FiveDaysData = {
           cityName: city.city,
           date: apiData.current.dt,
-          daily: days
+          daily: days,
+          percentprec: Math.floor(100-days.reduce((total, today)=>total*(1-today.percentprec/100), 100)),
+          total_rain: days.reduce((total, today)=>(today.expected_rain>0 && total >=0) ? total+today.expected_rain:-1, 1),
+          total_snow: days.reduce((total, today)=>(today.expected_snow>0 && total >=0) ? total+today.expected_snow:-1, 1)
         };
 
         res.status(200).json(data);
