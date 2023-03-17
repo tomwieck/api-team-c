846bf9459f7677fa4e9dae4d44a1b74db02a13f1
deemyboy <deemyboy2@gmail.com>
2023-03-17 15:23:51 +0000

    renamed to ForecastDayCard.tsx to forecast_day_card.tsx for frontend file naming convention

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,60 +2,65 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 
-import ForecastCity from "./components/forecast/city_forecast";
+import CityForecast from "./components/forecast/city_forecast";
 
 import {
-  Button,
-  Container,
-  Row,
-  Nav,
-  Navbar,
-  NavDropdown,
+    Button,
+    Container,
+    Row,
+    Nav,
+    Navbar,
+    NavDropdown,
 } from "react-bootstrap";
 import { CitySearch } from "./components/city_search/city_search";
 import { CityTable, ICityTableProps } from "./components/city_table/city_table";
 import { ICityRowProps } from "./components/city_row/city_row";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">API Team C</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <h3>Compare the weather for the next 5 days</h3>
-      <Container>
-        <Row>
-          <CitySearch />
-        </Row>
-        <Row>
-          <CityTable />
-        </Row>
-        <ForecastCity />
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">API Team C</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown
+                                title="Dropdown"
+                                id="basic-nav-dropdown"
+                            >
+                                <NavDropdown.Item href="#action/3.1">
+                                    Action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">
+                                    Something
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <h3>Compare the weather for the next 5 days</h3>
+            <Container>
+                <Row>
+                    <CitySearch />
+                </Row>
+                <Row>
+                    <CityTable />
+                </Row>
+                <CityForecast />
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/components/forecast/city_forecast.tsx
+++ frontend/src/components/forecast/city_forecast.tsx
@@ -1,35 +1,35 @@
-import ForecastDayCard from "./ForecastDayCard";
+import ForecastDayCard from "./forecast_day_card";
 import { useState } from "react";
 import { cities } from "../../dummy/dummy_data";
 
-const ForecastCity: React.FC = () => {
-  const [activeCard, setActiveCard] = useState(0);
-  // hardcode
-  const cityName = "London";
-  // dummy data
-  const cityWeather = cities.find(city => city.cityName === cityName);
+const CityForecast: React.FC = () => {
+    const [activeCard, setActiveCard] = useState(0);
+    // hardcode
+    const cityName = "London";
+    // dummy data
+    const cityWeather = cities.find((city) => city.cityName === cityName);
 
-  const updateActiveCard = (index: number) => {
-    setActiveCard(index);
-  };
+    const updateActiveCard = (index: number) => {
+        setActiveCard(index);
+    };
 
-  return (
-    <>
-      <div className="mt-40">
-        <h3 className="city">{cityName}</h3>
-        <div className="forecast-day-container">
-          {cityWeather &&
-            cityWeather.forecasts.map((forecast, index) => (
-              <ForecastDayCard
-                forecast={forecast}
-                isActive={index === activeCard}
-                toggleActive={() => updateActiveCard(index)}
-              />
-            ))}
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="mt-40">
+                <h3 className="city">{cityName}</h3>
+                <div className="forecast-day-container">
+                    {cityWeather &&
+                        cityWeather.forecasts.map((forecast, index) => (
+                            <ForecastDayCard
+                                forecast={forecast}
+                                isActive={index === activeCard}
+                                toggleActive={() => updateActiveCard(index)}
+                            />
+                        ))}
+                </div>
+            </div>
+        </>
+    );
 };
 
-export default ForecastCity;
+export default CityForecast;
