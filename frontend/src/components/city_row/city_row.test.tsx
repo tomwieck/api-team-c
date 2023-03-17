dbcd52f257a8e160f2ab92df1a988c2126e8f537
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-03-17 14:59:58 +0200

    rename icon_url for icon_id

--- frontend/src/components/city_row/city_row.test.tsx
+++ frontend/src/components/city_row/city_row.test.tsx
@@ -3,22 +3,22 @@ import { render, screen } from "@testing-library/react";
 import { CityRow, ICityRowProps } from "./city_row";
 
 test("renders city name ", () => {
-    const cityRowData: ICityRowProps = {
-        cityName: "London",
-        weatherIcons: [{ imgUrl: "windy" }, { imgUrl: "sunny" }],
-        forecasts: [
-            {
-                date_str: "Today", // 15th, 16th...
-                icon_url: `10d`,
-                temp_day_min: "9°",
-                temp_day_max: "12°",
-                wind_speed: "4m/s",
-                humidity: 51,
-                weather_description: "broken clouds", // could be used for selected forecast card
-            },
-        ],
-    };
-    // render(<CityRow {...cityRowData} />);
-    // const cityName = screen.getByText(/London/i);
-    // expect(cityName).toBeInTheDocument();
+  const cityRowData: ICityRowProps = {
+    cityName: "London",
+    weatherIcons: [{ imgUrl: "windy" }, { imgUrl: "sunny" }],
+    forecasts: [
+      {
+        date_str: "Today", // 15th, 16th...
+        icon_id: `10d`,
+        temp_day_min: "9°",
+        temp_day_max: "12°",
+        wind_speed: "4m/s",
+        humidity: 51,
+        weather_description: "broken clouds", // could be used for selected forecast card
+      },
+    ],
+  };
+  // render(<CityRow {...cityRowData} />);
+  // const cityName = screen.getByText(/London/i);
+  // expect(cityName).toBeInTheDocument();
 });
--- frontend/src/components/city_table/city_table.tsx
+++ frontend/src/components/city_table/city_table.tsx
@@ -8,41 +8,41 @@ import { ICityRowProps } from "../city_row/city_row";
 import { cities } from "../../dummy/dummy_data";
 
 export interface ICityTableProps {
-    cities: ICityRowProps[];
-    forecasts: IForecast[];
+  cities: ICityRowProps[];
+  forecasts: IForecast[];
 }
 
 export interface IForecast {
-    date_str: string;
-    icon_url: string;
-    temp_day_min: string;
-    temp_day_max: string;
-    wind_speed: string;
-    humidity: number;
-    weather_description: string;
+  date_str: string;
+  icon_id: string;
+  temp_day_min: string;
+  temp_day_max: string;
+  wind_speed: string;
+  humidity: number;
+  weather_description: string;
 }
 
 export interface IIsOpen {
-    1: boolean;
-    2: boolean;
-    3: boolean;
-    4: boolean;
-    5: boolean;
+  1: boolean;
+  2: boolean;
+  3: boolean;
+  4: boolean;
+  5: boolean;
 }
 
 export const CityTable: React.FC = () => {
-    const [open, setOpen]: IIsOpen = {
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-        5: false,
-    };
-    return (
-        <div className="city-table col">
-            {cities.map((city, index) => (
-                <CityRow key={index} {...city} onClick={setOpen(!open)} />
-            ))}
-        </div>
-    );
+  // const [open, setOpen]: IIsOpen = {
+  //     1: false,
+  //     2: false,
+  //     3: false,
+  //     4: false,
+  //     5: false,
+  // };
+  return (
+    <div className="city-table col">
+      {cities.map((city, index) => (
+        <CityRow key={index} {...city} />
+      ))}
+    </div>
+  );
 };
