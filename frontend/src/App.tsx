c82e3ea86d2da3b5c48e46f3d8f52a0013f4bb5a
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-03-18 00:41:51 +0200

    (ref): remove unnecessary import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,52 +4,47 @@ import "./App.scss";
 
 import { Container, Row, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { CitySearch } from "./components/city_search/city_search";
-import { CityTable, ICityTableProps } from "./components/city_table/city_table";
+import { CityTable } from "./components/city_table/city_table";
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">API Team C</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown
-                                title="Dropdown"
-                                id="basic-nav-dropdown"
-                            >
-                                <NavDropdown.Item href="#action/3.1">
-                                    Action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">
-                                    Something
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <h3>Compare the weather for the next 5 days</h3>
-            <Container>
-                <Row>
-                    <CitySearch />
-                </Row>
-                <Row>
-                    <CityTable />
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">API Team C</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <h3>Compare the weather for the next 5 days</h3>
+      <Container>
+        <Row>
+          <CitySearch />
+        </Row>
+        <Row>
+          <CityTable />
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
