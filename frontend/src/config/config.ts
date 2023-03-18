339c24d5eb3e2a34db4272ec6e723177490a2423
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-03-18 15:03:54 +0200

    (feat): add useFetchData hook

---
+++ frontend/src/config/config.ts
@@ -0,0 +1,4 @@
+export const API_BASE_URL = "http://localhost:5000";
+export const API_CITIES_URL = `${API_BASE_URL}/cities`;
+export const API_CITY_FORECAST_FOR_1_DAY = `${API_BASE_URL}/city/1`;
+export const API_CITY_FORECAST_FOR_5_DAYS = `${API_BASE_URL}/city/5`;
