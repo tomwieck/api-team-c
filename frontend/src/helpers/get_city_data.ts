e2a5d320dc45557828829fca8de3b763f87813be
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-03-21 09:19:56 +0200

    (feat): format data for the CityForecastComponent

--- frontend/src/helpers/get_city_data.ts
+++ frontend/src/helpers/get_city_data.ts
@@ -3,18 +3,19 @@ import { IForecast, IForecastCity } from "../types/forecast_frontend.types";
 
 const getDate = (index: number) => {
   if (index === 0) {
-    return 'Today';
+    return "Today";
   }
   let newDate = new Date();
   newDate.setDate(newDate.getDate() + index);
-  let stringDate = newDate.toLocaleDateString();
-  let dateParts = stringDate.split('/');
+  const day = newDate.getDate();
+  const weekday = newDate.toLocaleString("en-us", { weekday: "short" });
+  return `${weekday} ${day}${getOrdinal(day)}`;
+};
 
-  return dateParts[0] + '-' + dateParts[1];
-}
-
-const get_day_forecast_data = (dayForecast: OneDaysData, index: number): IForecast => {
-  console.log(index)
+const get_day_forecast_data = (
+  dayForecast: OneDaysData,
+  index: number
+): IForecast => {
   return {
     date_str: getDate(index),
     icon_id: dayForecast.icon_id,
@@ -31,8 +32,10 @@ export const get_city_data = (
 ): IForecastCity | undefined => {
   if (data !== undefined) {
     const daily_data = data.daily.slice(0, 5);
-    const forecasts = daily_data.map((forecast, index) => get_day_forecast_data(forecast, index));
-    const weatherIcons = forecasts.map((forecast) => ({
+    const forecasts = daily_data.map((forecast, index) =>
+      get_day_forecast_data(forecast, index)
+    );
+    const weatherIcons = forecasts.map(forecast => ({
       iconId: forecast.icon_id,
     }));
     return {
@@ -42,3 +45,17 @@ export const get_city_data = (
     };
   }
 };
+
+function getOrdinal(n: number) {
+  let ord = "th";
+
+  if (n % 10 === 1 && n % 100 !== 11) {
+    ord = "st";
+  } else if (n % 10 === 2 && n % 100 !== 12) {
+    ord = "nd";
+  } else if (n % 10 === 3 && n % 100 !== 13) {
+    ord = "rd";
+  }
+
+  return ord;
+}
